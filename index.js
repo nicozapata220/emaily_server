2b9c5d9cbb13d20743cd0c08b1aec44adffd9eb5
nicozapata220 <nicolas.a.zapata@gmail.com>
2018-03-18 11:35:31 -0300

    Everything until setting up mongoose

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const app = express();
+//npm run dev to run the app
+const express = require("express");
+const mongoose = require("mongoose");
+const passportConfig = require("./services/passport"); //como no este require no devuelve nada, y no hace falta que lo tengamos como variable, es lo mismo si ponemos solo el require statement, sin asignarlo a la const
+const authRoutes = require("./routes/authRoutes");
+const keys = require("./config/keys");
 
-app.get('/', (req,res) => {
-    res.send({hi: 'Hola hola!'});
-})
+//hago la conexión a la DB
+mongoose.connect(keys.mongoURI)
+
+const app = express();
+/*Un refactor que se puede hacer ahí es require("./routes/authRoutes")(app); eliminando la declaración de la constante*/
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
